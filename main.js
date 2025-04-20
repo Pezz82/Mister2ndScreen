34591e0aec9f95c18bcb35b216643fa512f02e45
Pezz82 <68499877+Pezz82@users.noreply.github.com>
2025-04-20 21:37:26 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,40 @@
+// ----------------------------
+// File: main.js
+// ----------------------------
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 900,
+    height: 600,
+    autoHideMenuBar: true,     // hide default menu bar
+    resizable: true,
+    show: false,               // wait to show until ready
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
+
+  // Load the built React app (docs/index.html)
+  win.loadFile(path.join(__dirname, 'docs', 'index.html'));
+
+  win.once('ready-to-show', () => {
+    win.show();
+  });
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
+
